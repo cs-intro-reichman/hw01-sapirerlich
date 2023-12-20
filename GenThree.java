35989626ab3873c9f2b341da237326d25c9248f5
sapirerlich <sapir.erlich@runi.ac.il>
2023-12-20 18:23:04 +0200

    Add awesome code for Homework 1

--- GenThree.java
+++ GenThree.java
@@ -5,8 +5,10 @@
 public class GenThree {
 	public static void main(String[] args) {
 		// Declares 2 integer variables for the min and max of range, and sets them according to the command line argument and the min and max func
-		int min_range = Math.min(Integer.parseInt(args[0]),Integer.parseInt(args[1]));
-		int max_range = Math.max(Integer.parseInt(args[0]),Integer.parseInt(args[1]));
+		int min_range = Math.min(Integer.parseInt(args[0])
+		,Integer.parseInt(args[1]));
+		int max_range = Math.max(Integer.parseInt(args[0])
+		,Integer.parseInt(args[1]));
 		//Calculates the range (not includes the max_range number)
 		int range = max_range - min_range;
 		// Randomize 3 integers in the range
